6eb19f99cd45422ebdd099834431e20436b41217
Justin Bellamy <contact@justinbellamy.com>
2016-06-15 03:03:01 -0700

    Updated to a simple app
    Moved stuff around

--- web/index.php
+++ web/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__ . '/../src/app.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
